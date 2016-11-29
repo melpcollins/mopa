60096badbf035a150282f5c494c9f2066edfa0d9
melpcollins <melpcollins@gmail.com>
2016-11-29 12:40:38 +0000

    new function

--- R/cat_function.R
+++ R/cat_function.R
@@ -14,3 +14,19 @@ cat_function <- function(love=TRUE){
     print("I am not a cool person.")
   }
 }
+#' A Mel Function
+#'
+#' This function allows you to express your love of Mel
+#' @param love Do you love Mel? Defaults to TRUE.
+#' @keywords Mel
+#' @export
+#' @examples
+#' mel_function()
+mel_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love mel!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
