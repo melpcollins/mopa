aaf1d36b2ca11c365ee49061e9371d081c09db5a
melpcollins <melpcollins@gmail.com>
2016-11-29 16:22:28 +0000

    added second test
    added second test for Mel_function

--- tests/testthat/test.mel_function.R
+++ tests/testthat/test.mel_function.R
@@ -1,3 +1,6 @@
 test_that("mel_function returns Love",{
   expect_that(mel_function(),prints_text("I love mel!"))
 })
+test_that("mel_function returns Love",{
+  expect_that(mel_function(love=FALSE),prints_text("I am not a cool person."))
+})
